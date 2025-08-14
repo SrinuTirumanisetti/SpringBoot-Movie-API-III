17e4e1b8639bd1c713874b63b0713d6be0bc9cea
SrinuTirumanisetti <srinutirumanisetti@gmail.com>
2025-08-14 17:49:08 +0000

    Created Movie model entity

--- src/main/java/com/example/movie/model/Movie.java
+++ src/main/java/com/example/movie/model/Movie.java
@@ -6,3 +6,56 @@
  */
 
 // Write your code here
+
+/*
+ * You can use the following import statements
+ * 
+ * import javax.persistence.*;
+ * 
+ */
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "movielist")
+public class Movie {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int movieId;
+    private String movieName;
+    private String leadActor;
+
+    public Movie() {
+    }
+
+    public Movie(int movieId, String movieName, String leadActor) {
+        this.movieId = movieId;
+        this.movieName = movieName;
+        this.leadActor = leadActor;
+    }
+
+    public int getMovieId() {
+        return movieId;
+    }
+
+    public void setMovieId(int movieId) {
+        this.movieId = movieId;
+    }
+
+    public String getMovieName() {
+        return movieName;
+    }
+
+    public void setMovieName(String movieName) {
+        this.movieName = movieName;
+    }
+
+    public String getLeadActor() {
+        return leadActor;
+    }
+
+    public void setLeadActor(String leadActor) {
+        this.leadActor = leadActor;
+    }
+}
